9d6e505f1a393fba87f164b108056640e435b23f
Shubham Mane <mane6shubham@gmail.com>
2022-05-18 17:51:27 +0530

    First Commit

---
+++ src/main/java/netjavaguides/springboot/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package netjavaguides.springboot.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import netjavaguides.springboot.model.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
